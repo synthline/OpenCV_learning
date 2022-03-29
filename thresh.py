667e94de11725998b80533aa01f6d0c5f9a44667
synthline <mso.accts.af@outlook.com>
2022-03-29 08:38:28 +0200

    4

--- thresh.py
+++ thresh.py
@@ -11,10 +11,13 @@ cv.imshow('Gray', gray)
 threshold, thresh = cv.threshold(gray, 100, 255, cv.THRESH_BINARY)
 cv.imshow('Simple Threshold', thresh)
 
-
+# inverts the thershold
 threshold, thresh_inv = cv.threshold(gray, 100, 255, cv.THRESH_BINARY_INV)
 cv.imshow('Simple Threshold Inverse', thresh_inv)
 
+# Adaptive Thresholding  (can do the same with the inverse as well :D)
+adaptive_thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 3)
+cv.imshow('Adaptive Threshold', adaptive_thresh)
 
 
 cv.waitKey(0)
