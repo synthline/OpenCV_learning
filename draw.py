cf4bcd9401c86f7a1089cb2312daca0e0d37f93e
synthline <mso.accts.af@outlook.com>
2022-03-30 10:19:36 +0200

    1

--- draw.py
+++ draw.py
@@ -6,7 +6,7 @@ import numpy as np
 
 #blank image
 blank = np.zeros((500,500,3), dtype='uint8')
-# cv.imshow('Blank', blank)
+cv.imshow('Blank', blank)
 
 #Paint the image in a certain colour
 # blank[:] = 0,255,0
@@ -16,9 +16,9 @@ blank = np.zeros((500,500,3), dtype='uint8')
 # blank[200:300, 300:400] = 0,255,0
 # cv.imshow('Green', blank)
  
-#Draw a Line
-# cv.line(blank, (0,0), (blank.shape[1]//2,blank.shape[0]//2), (0,255,0), thickness=1)
-# cv.imshow('Line', blank)
+# Draw a Line
+cv.line(blank, (0,0), (blank.shape[1]//2,blank.shape[0]//2), (0,255,0), thickness=1)
+cv.imshow('Line', blank)
 
 #Draw a rectangle
 # cv.rectangle(blank, (0,0), (250,250), (0,255,0), thickness=2)
--- gradients.py
+++ gradients.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 
 
-img = cv.imread('photos/cat.jpg')
+img = cv.imread('photos/qDhl7.jpg')
 cv.imshow('Cat', img)
 
 # FROM BASIC:
